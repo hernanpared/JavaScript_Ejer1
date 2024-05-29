38a2e5094a15bdd60fc90ab04cac2a06047c466b
ruben hernan pared <hernan2014@hotmail.com>
2024-05-29 09:44:21 -0300

    tp_ejercicio1

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('square1').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'red';
+});
+
+document.getElementById('square2').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'green';
+});
+
+document.getElementById('square3').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'blue';
+});
